6253e3dd2635a462e9df3784f81203ab1a5291e5
William Sedlacek <wsedlacekc@gmail.com>
2020-03-23 11:57:50 -0500

    LS-8: Setup CPU for print8.ls8
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,7 +8,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = {}
+        self.reg = {}
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -28,14 +30,26 @@ class CPU:
         ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.ram_write(address, instruction)
             address += 1
 
-    def alu(self, op, reg_a, reg_b):
+    def ram_read(self, address: int):
+        return self.ram[address]
+
+    def ram_write(self, address: int, value: int):
+        self.ram[address] = value
+
+    def reg_read(self, register: int):
+        return self.reg[register]
+
+    def reg_write(self, register: int, value: int):
+        self.reg[register] = value
+
+    def alu(self, op: str, reg_a: int, reg_b: int):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.reg_write(reg_a, self.reg[reg_a] + self.reg[reg_b])
         # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -48,8 +62,6 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            self.fl,
-            self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +74,23 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        while True:
+            instruction = self.ram_read(self.pc)
+
+            if instruction == 0b10000010:  # LDI
+                reg = self.ram_read(self.pc + 1)
+                value = self.ram_read(self.pc + 2)
+                self.reg_write(reg, value)
+                self.pc += 2
+
+            if instruction == 0b01000111:  # PRN
+                reg = self.ram_read(self.pc + 1)
+                value = self.reg_read(reg)
+                print(value)
+                self.pc += 1
+
+            if instruction == 0b00000001:  # HLT
+                break
+
+            self.pc += 1
